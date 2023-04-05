dafecc2571fbadff86069a6b3353d6164217574f
Fielding219 <Featherston.12@osu.edu>
2023-04-05 11:14:29 -0400

    Catalog Commit

---
+++ Ice.Owl.Domain/Catalog/CatalogController.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+using Ice.Owl.Domain.Catalog;
+
+
+namespace Ice.Owl.Api.Controllers{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase {
+
+        [HttpGet]
+        public IActionResult GetItems() {
+            
+            var items = new List<Item>(){
+               new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+               new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m) 
+            };
+           return Ok(items);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Getitem(int id){
+            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            item.ID = id;
+
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Item item){
+            return Created("/catalog/42", item);
+        }
+
+        [HttpPost("{id:int}/ratings")]
+        public IActionResult PostRating(int id, [FromBody] Rating rating){
+
+            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            item.ID = id;
+            item.AddRating(rating);
+
+            return Ok(item);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id, Item item){
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id){
+            return NoContent();
+        }
+
+    }
+    
+}
---
+++ Ice.Owl.Domain/Catalog/Rating.cs
@@ -0,0 +1,21 @@
+namespace Ice.Owl.Domain.Catalog {
+     public class Rating {
+        public int Stars {get; set;}
+        public string UserName {get; set;}
+        public string Review {get; set;}
+    
+
+    public Rating(int stars, string userName, string review) {
+        if(Stars < 1 || Stars > 5){
+            throw new ArgumentException("Star rating must be an integer of: 1, 2, 3, 4, 5.");
+        }
+
+        if(string.IsNullOrEmpty(UserName)){
+            throw new ArgumentException("UserName cannot be null");
+        }
+        this.Stars = stars;
+        this.UserName = userName;
+        this.Review = review;
+    }
+ }
+}
