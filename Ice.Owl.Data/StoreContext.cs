2a748ec821643fa45b00e2a80e4d84596e518959
Fielding219 <Featherston.12@osu.edu>
2023-04-10 00:06:27 -0400

    StoreContext Class

---
+++ Ice.Owl.Data/StoreContext.cs
@@ -0,0 +1,14 @@
+using Ice.Owl.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ice.Owl.Data{
+
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+            {}
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
