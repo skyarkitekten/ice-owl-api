39471b93ec4f28d7ffb0b78d0c0b5c5890b6c944
ELIZABETH PERELSHTEYN <PERELSHTEYN.1@osu.edu>
2023-04-11 11:44:00 -0400

    debug

---
+++ Ice.Owl.Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Ice.Owl.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ice.Owl.Data {
+
+
+public class StoreContext : DbContext 
+{
+    public StoreContext(DbContextOptions<StoreContext> options)
+     : base(options)
+     { }
+
+public DbSet<Item>Items {get; set;}
+    }
+}
